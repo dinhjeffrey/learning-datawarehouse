3d8c7e371d1001670b766911db1000bf97b955d6
jeffrey dinh <dinhjeffreyy@gmail.com>
2017-02-26 14:31:19 -0800

    aiotestking

---
+++ aiotestking.sql
@@ -0,0 +1,5 @@
+/*
+You need to ensure that users can update only the phone numbers and email addresses by using this view. What should you do?
+
+Create an INSTEAD OF UPDATE trigger on the view
+*/
